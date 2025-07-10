d990352e5e9770daa9487ef04216289befa1ff34
León g <legarcdi@gmail.com>
2025-07-09 22:43:09 -0600

    inicializacion de proyecto ya con webview

---
+++ app/src/main/java/com/t91s/webview/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.t91s.webview
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            cacheMode = WebSettings.LOAD_DEFAULT
+        }
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://t91s.adcotec.com.mx")
+    }
+}
